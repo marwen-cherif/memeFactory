e449cd85327b9a4b06ef80529d98125452b6bb2d
Marwen Cherif <m.cherif@revers.io>
2024-10-25 22:14:56 +0100

    Optimize refresh call after meme and comment creation

--- src/pages/CreateMemePage/CreateMemePage.hooks.ts
+++ src/pages/CreateMemePage/CreateMemePage.hooks.ts
@@ -2,19 +2,47 @@ import { CreateMemeFormValues } from '@/pages/CreateMemePage/CreateMemePage.type
 import { useForm } from 'react-hook-form';
 import { Service } from '@/api';
 import { useNavigate } from '@tanstack/react-router';
+import { useQueryClient } from '@tanstack/react-query';
+import { GET_MEMES_QUERY_KEY } from '@/pages/MemeFeedPage/hooks/useGetMemes.ts';
+import { Meme } from '@/api/api.ts';
 
 export const useCreateMemePage = () => {
   const formContext = useForm<CreateMemeFormValues>();
   const { handleSubmit } = formContext;
-
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const handleCreateMeme = handleSubmit(async (data) => {
-    await Service.createMeme({
+    const meme = await Service.createMeme({
       description: data.description,
       picture: data.picture,
       texts: data.texts,
     });
+
+    queryClient.setQueryData<{ pages: { results: Meme[] }[] }>(
+      [GET_MEMES_QUERY_KEY],
+      (oldData) => {
+        if (!oldData) {
+          return {
+            pages: [
+              {
+                results: [meme],
+              },
+            ],
+          };
+        } else {
+          return {
+            ...oldData,
+            pages: oldData.pages.map((page) => {
+              return {
+                ...page,
+                results: [meme, ...page.results],
+              };
+            }),
+          };
+        }
+      }
+    );
 
     navigate({
       to: '/',
--- src/pages/MemeFeedPage/MemeFeedItem/MemeFeedItemComments/SubmitNewComment/hooks/useCreateMemeComment.ts
+++ src/pages/MemeFeedPage/MemeFeedItem/MemeFeedItemComments/SubmitNewComment/hooks/useCreateMemeComment.ts
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { Service } from '@/api';
 import { useForm } from 'react-hook-form';
 import { GET_MEME_COMMENT_QUERY_KEY } from '@/pages/MemeFeedPage/MemeFeedItem/MemeFeedItemComments/hooks/useGetMemeComments.ts';
+import { MemeComment } from '@/api/api.ts';
 
 export const useCreateMemeComment = ({ memeId }: { memeId: string }) => {
   const formContext = useForm<{ content: string }>({
@@ -15,8 +16,35 @@ export const useCreateMemeComment = ({ memeId }: { memeId: string }) => {
     mutationFn: async (data: { memeId: string; content: string }) => {
       return await Service.createMemeComment(data.memeId, data.content);
     },
-    onSuccess: async () => {
-      await queryClient.invalidateQueries({ queryKey: [GET_MEME_COMMENT_QUERY_KEY, { memeId }] });
+    onSuccess: async (memeComment) => {
+      queryClient.setQueryData<{ pages: { results: MemeComment[] }[] }>(
+        [GET_MEME_COMMENT_QUERY_KEY, { memeId }],
+        (oldData) => {
+          if (!oldData) {
+            return {
+              pages: [
+                {
+                  results: [memeComment],
+                },
+              ],
+            };
+          } else {
+            return {
+              ...oldData,
+              pages: oldData.pages.map((page) => {
+                return {
+                  ...page,
+                  results: [memeComment, ...page.results],
+                };
+              }),
+            };
+          }
+        }
+      );
+
+      await queryClient.invalidateQueries({
+        queryKey: [GET_MEME_COMMENT_QUERY_KEY, { memeId }],
+      });
     },
   });
 
